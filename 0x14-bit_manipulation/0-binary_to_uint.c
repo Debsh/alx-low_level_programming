d93e554e9a4a9d1d376cbc37b6147215d41f09fd
Debora Taye <dibotaye3@gmail.com>
2022-04-28 11:53:38 -0700

    trying to fix the NULL error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -9,17 +10,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int len = 0;
+	unsigned int num = 0, mult = 1;
+	int len;
 
-	if (b[len] == '\0')
+	if (b == '\0')
 		return (0);
 
-	while ((b[len] == '0') || (b[len] == '1'))
-	{
-		num <<= 1;
-		num += b[len] - '0';
+	for (len = 0; b[len];)
 		len++;
+
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		num += (b[len] - '0') * mult;
+		mult *= 2;
 	}
 
 	return (num);
