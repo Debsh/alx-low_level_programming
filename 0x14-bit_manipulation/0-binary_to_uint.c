94da7a360076fe33738a9004bc457e94128e7374
Debora Taye <dibotaye3@gmail.com>
2022-04-28 11:55:43 -0700

    task o

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 /**
@@ -10,22 +9,17 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int num = 0;
+	int len = 0;
 
-	if (b == '\0')
+	if (b[len] == '\0')
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	while ((b[len] == '0') || (b[len] == '1'))
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		num <<= 1;
+		num += b[len] - '0';
+		len++;
 	}
 
 	return (num);
