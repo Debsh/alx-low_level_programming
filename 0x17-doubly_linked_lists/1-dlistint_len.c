939fb4621311dd10c881dae4e508a48364958369
Debora Taye <dibotaye3@gmail.com>
2022-06-09 10:55:16 -0700

    List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,16 +5,17 @@ nclude "lists.h"
  *   * @h: The head of the dlistint_t list.
  *    *
  *     * Return: The number of elements in the dlistint_t list.
- *      */
+ * /
 size_t dlistint_len(const dlistint_t *h)
 {
-		size_t nodes = 0;
+         
+       size_t i = 0;
 
-			while (h)
-					{
-								nodes++;
-										h = h->next;
-											}
+       while (h != NULL)
+       {
 
-				return (nodes);
+               i++;
+	       h = h->next;
+       }
+       return (i);
 }
