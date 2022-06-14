65e40cf94697211afa0ac2e7556efb6f94e105f6
Debora Taye <dibotaye3@gmail.com>
2022-06-14 13:41:02 -0700

    fix

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,13 +9,13 @@ nclude "lists.h"
 size_t dlistint_len(const dlistint_t *h)
 {
          
-       size_t i = 0;
+       size_t nodes = 0;
 
-       while (h != NULL)
+       while (h)
        {
+              nodes++;
+	      h = h->next;
+      }
 
-               i++;
-	       h = h->next;
-       }
-       return (i);
+      return (nodes);
 }
